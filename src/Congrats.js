a88ddb703c1c126c1eefbee6612a2f06d5eead5f
Lebedev Boris <lebedev.moscow.me@gmail.com>
2020-10-13 17:38:35 +0500

    Add some test (but not add the component for them itslef

---
+++ src/Congrats.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/**
+ * Functional React component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered Component
+ */
+
+ export default () => {
+     return (
+         <div />
+     )
+ }
