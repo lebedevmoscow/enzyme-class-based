185eafdcf66af3f6ba4585d671b504f20e2a411a
Lebedev Boris <lebedev.moscow.me@gmail.com>
2020-10-14 16:05:36 +0500

    Add setupTest file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow} from 'enzyme'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without error', () => {
+  const app = shallow(<App />);
+  expect(app.length).not.toBe(0)
 });
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Enzyme, {shallow, ShallowWrapper} from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import {shallow, ShallowWrapper} from 'enzyme'
 import Congrats from './Congrats'
 import {findByTestAttr, checkProps} from './../test/testUtils'
 
-
-Enzyme.configure({adapter: new EnzymeAdapter()})
-
+const defaultProps = {success: false}
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
@@ -15,7 +12,8 @@ Enzyme.configure({adapter: new EnzymeAdapter()})
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-    return shallow(<Congrats {...props} />)
+    const setupProps = {...defaultProps, ...props}
+    return shallow(<Congrats {...setupProps} />)
 }
 
 test('renders without error', () => {
