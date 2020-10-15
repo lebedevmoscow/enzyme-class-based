2a411adfd1087cda3b8f5d9360b99964b6a62fcd
Lebedev Boris <lebedev.moscow.me@gmail.com>
2020-10-15 17:35:28 +0500

    Set up guessWords tests

---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function guessedWordsReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - Action to be reduced
+ * @returns {array} - New guessed state
+ */
+
+export default (state, action) => {
+    return null
+}
